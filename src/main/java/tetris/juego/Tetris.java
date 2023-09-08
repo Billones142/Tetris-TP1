2d2d71fc798f6970efe7d917909fa6b2b15b0a32
Billones142 <stefanomerinoderui@gmail.com>
2023-09-08 15:26:57 -0300

    08-09 15:26

--- src/main/java/tetris/juego/Tetris.java
+++ src/main/java/tetris/juego/Tetris.java
@@ -1,9 +1,26 @@
 package tetris.juego;
 
 public class Tetris {
-    public void start(){
+    public Clock clock = new Clock();
+    
+
+    public void start(){ //TODO
         
     }
 
+    public void state(){ //TODO
 
+    }
+
+    public rotateLeft(){ //TODO
+
+    }
+
+    public rotateRight(){ //TODO
+
+    }
+
+    public void tick(){ 
+        clock.tick();
+    }
 }
